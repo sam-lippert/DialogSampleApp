752dd26d5c7299e08fe61c818eef88cbac95e6d8
Sam Lippert <lippertz@gmail.com>
2012-07-03 09:38:42 -0500

    Better sharing of resources between API levels, running on 1.6

--- Resources/Resource.Designer.cs
+++ Resources/Resource.Designer.cs
@@ -46,59 +46,59 @@ namespace DialogSampleApp
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f05000f
-			public const int button_bar = 2131034127;
+			// aapt resource value: 0x7f06000f
+			public const int button_bar = 2131099663;
 			
-			// aapt resource value: 0x7f050010
-			public const int button_done = 2131034128;
+			// aapt resource value: 0x7f060010
+			public const int button_done = 2131099664;
 			
-			// aapt resource value: 0x7f050011
-			public const int button_revert = 2131034129;
+			// aapt resource value: 0x7f060011
+			public const int button_revert = 2131099665;
 			
-			// aapt resource value: 0x7f050005
-			public const int dialog_BoolField = 2131034117;
+			// aapt resource value: 0x7f060005
+			public const int dialog_BoolField = 2131099653;
 			
-			// aapt resource value: 0x7f050006
-			public const int dialog_Button = 2131034118;
+			// aapt resource value: 0x7f060006
+			public const int dialog_Button = 2131099654;
 			
-			// aapt resource value: 0x7f05000a
-			public const int dialog_DisclosureField = 2131034122;
+			// aapt resource value: 0x7f06000a
+			public const int dialog_DisclosureField = 2131099658;
 			
-			// aapt resource value: 0x7f050009
-			public const int dialog_HtmlField = 2131034121;
+			// aapt resource value: 0x7f060009
+			public const int dialog_HtmlField = 2131099657;
 			
-			// aapt resource value: 0x7f050007
-			public const int dialog_ImageLeft = 2131034119;
+			// aapt resource value: 0x7f060007
+			public const int dialog_ImageLeft = 2131099655;
 			
-			// aapt resource value: 0x7f050003
-			public const int dialog_ImageRight = 2131034115;
+			// aapt resource value: 0x7f060003
+			public const int dialog_ImageRight = 2131099651;
 			
-			// aapt resource value: 0x7f050000
-			public const int dialog_LabelField = 2131034112;
+			// aapt resource value: 0x7f060000
+			public const int dialog_LabelField = 2131099648;
 			
-			// aapt resource value: 0x7f050004
-			public const int dialog_LabelPercentageField = 2131034116;
+			// aapt resource value: 0x7f060004
+			public const int dialog_LabelPercentageField = 2131099652;
 			
-			// aapt resource value: 0x7f050002
-			public const int dialog_LabelSubtextField = 2131034114;
+			// aapt resource value: 0x7f060002
+			public const int dialog_LabelSubtextField = 2131099650;
 			
-			// aapt resource value: 0x7f05000b
-			public const int dialog_Panel = 2131034123;
+			// aapt resource value: 0x7f06000b
+			public const int dialog_Panel = 2131099659;
 			
-			// aapt resource value: 0x7f05000c
-			public const int dialog_RadioButtonList = 2131034124;
+			// aapt resource value: 0x7f06000c
+			public const int dialog_RadioButtonList = 2131099660;
 			
-			// aapt resource value: 0x7f050008
-			public const int dialog_SliderField = 2131034120;
+			// aapt resource value: 0x7f060008
+			public const int dialog_SliderField = 2131099656;
 			
-			// aapt resource value: 0x7f05000d
-			public const int dialog_Spinner = 2131034125;
+			// aapt resource value: 0x7f06000d
+			public const int dialog_Spinner = 2131099661;
 			
-			// aapt resource value: 0x7f050001
-			public const int dialog_ValueField = 2131034113;
+			// aapt resource value: 0x7f060001
+			public const int dialog_ValueField = 2131099649;
 			
-			// aapt resource value: 0x7f05000e
-			public const int iFormFieldValue = 2131034126;
+			// aapt resource value: 0x7f06000e
+			public const int iFormFieldValue = 2131099662;
 			
 			private Id()
 			{
@@ -195,6 +195,17 @@ namespace DialogSampleApp
 			public const int hello = 2130968576;
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int ApplicationTheme = 2131034112;
+			
+			private Style()
 			{
 			}
 		}
